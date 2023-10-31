d22a0745813fdd4ebc3b10923806a0c24ee67feb
yaufdd <ericozavr@gmail.com>
2023-10-31 19:32:08 +0300

    student info saved into file and bot shows stundent group

--- main.py
+++ main.py
@@ -1,56 +1,67 @@
 import requests
-import datetime
 from bs4 import BeautifulSoup
+from urllib.parse import quote
+import json
 
 session = requests.Session()
 
-
-def authorization(login, password):
+def authorization():
+    with open('user_info.json', 'r') as file:
+        info = json.load(file)
 
     login_url = 'https://lk.mirea.ru/auth.php'
 
     login_data = {
         'AUTH_FORM' : 'Y',
         'TYPE' : 'AUTH',
-        'USER_LOGIN' : login,
-        'USER_PASSWORD' : password,
+        'USER_LOGIN' : info['login'],
+        'USER_PASSWORD' : info['password'],
         'USER_REMEMBER' : 'Y'
     }
-
     login_responce = session.post(login_url, data=login_data)
-
+    
     return login_responce
     
 
-def find_student_group(login, password):
-    
-    login_responce = authorization(login, password)
-    soup =  BeautifulSoup(login_responce.text, 'html.parser')
-    group_table = soup.find_all('table')[0]
+user_data = {} 
 
-    elements_in_gourp_table = group_table.find_all('td')
-    
-    student_gourp = elements_in_gourp_table[4]
+#Ищет группу студента и запоминает его в словаь
+def find_student_group():
+    try:
+        login_responce = authorization()
+        soup =  BeautifulSoup(login_responce.text, 'html.parser')
+        group_table = soup.find_all('table')[0]
 
-    print(f"Your group is {student_gourp.text}")
+        elements_in_gourp_table = group_table.find_all('td')
+        
+        student_gourp = elements_in_gourp_table[4]
+        user_data['group'] = student_gourp.text
+        return user_data
+    except IndexError:
+        return 
+
+#Создает специальную ссылку для студента
+def get_personal_url(url):
+    with open('user_info.json', 'r') as file:
+        student_group = json.load(file)
+    personal_url = url + quote(student_group['group'], encoding='utf-8')
+    return personal_url
 
 
-def parse_schedule(url):
+def parse_schedule_api():
     authorization()
-
-    schedule_responce = session.get(url)
-
-    file_path = "example.html"
-    with open(file_path, "w", encoding="utf-8") as file:
-        file.write(schedule_responce.text)
-    if "Облачные технологии" in schedule_responce:
-        print("ok")
-    print(schedule_responce.headers)
+    schedule_responce = session.get(get_personal_url('https://timetable.mirea.ru/api/groups/name/'))
+    schedule_api_data = json.loads(schedule_responce.text)
+    lessons = schedule_api_data["lessons"]
+    return lessons
 
 
+def make_schedule(data):
+    subject = {}
+    if data['weekday'] == 1:
+        print("True")
 
-
-
+print(parse_schedule_api())
 
 
 #https://timetable.mirea.ru/ui/schedule?group=%D0%91%D0%A1%D0%91%D0%9E-17-20
--- telegram_bot.py
+++ telegram_bot.py
@@ -5,11 +5,55 @@ token = '6728104105:AAGaffrRqVxbOKCT6wetjQDpZWFXmjGtA0s'
 
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['my_info'])  
-def get_stundent_login(message):
-    bot.send_message(message.chat.id, "Введите ваш логин: ")
-    login = message.text
 
 
-bot.polling(non_stop=True)
+@bot.message_handler(commands=["auth"])
+def auth(message): 
+    bot.send_message(message.chat.id, "Введите логин")
+    bot.register_next_step_handler(message, save_login)
 
+
+def save_login(message):
+    main.user_data['login'] = message.text
+    bot.send_message(message.chat.id, "Введите пароль") 
+    with open('user_info.json', 'w') as file:
+        main.json.dump(main.user_data, file)
+    bot.register_next_step_handler(message, save_password)
+
+#Запоминает пароль, входит в систему, парсит группу студента и запоминает
+def save_password(message):
+    main.user_data['password'] = message.text
+    with open('user_info.json', 'w') as file:
+        main.json.dump(main.user_data, file)
+    student_group = main.find_student_group()
+    if student_group != None:
+        with open('user_info.json', 'w') as file:
+            main.json.dump(student_group, file)
+        bot.send_message(message.chat.id, f"Ваша информация сохранена. Вы студент группы {student_group['group']}")
+    else:
+        bot.send_message(message.chat.id, "Данные введены неправильно. Запустите команду еще раз")
+
+#Показывает информацию группы
+@bot.message_handler(commands=["info"])
+def show_info(message):
+    with open ('user_info.json', 'r') as file:
+        info = main.json.load(file)
+    bot.send_message(message.chat.id, info['group'])
+
+
+
+@bot.message_handler(commands=['schedule'])
+def show_schedule(message):
+    currenct_url = main.get_personal_url('https://timetable.mirea.ru/api/groups/name/')
+    student_schedule = main.parse_schedule_api(currenct_url, main.user_data['login'], main.user_data['password'])
+    
+
+
+
+bot.polling(none_stop=True)
+
+#bamba.e@edu.mirea.ru
+#a8JkFUmW
+
+#sergeeva.u.m@edu.mirea.ru
+#SergeevaMike@19
